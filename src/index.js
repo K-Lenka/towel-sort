e71e14859d5152f7244ec3397430a6f31d9c4b07
k-lenka <alena1027@hmail.com>
2021-03-13 15:29:26 -0400

    feat: test solve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    var newMatrix;
+    if (!matrix || !matrix.length) {
+        return [];
+    }
+
+    for (let i = 1; i < matrix.length; i += 2) { // This prints the array element at the odd indices 
+        matrix[i].reverse();
+    }
+    newMatrix = [].concat(...matrix)
+    return newMatrix;
+
 }
